bcedf8dcbbfa621b9e091e24c305e8c70dec28c5
S-M-ZAHIDUR-RAHMAN <smz.xahid@gmail.com>
2023-11-11 19:10:57 +0600

    made homepage responsive

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
             <div className="flex flex-row justify-center mb-5 text-4xl text-white font-bold hero-overlay mt-5 py-4">
                 <h2>Features</h2>
             </div>
-            <div className="flex flex-col justify-center lg:flex lg:flex-row lg:justify-center gap-4">
+            <div className="flex flex-col items-center lg:flex lg:flex-row lg:justify-center gap-4">
             {
                 parts?.map((part) =>
                  <Feature key={part?._id} part={part} ></Feature>)
@@ -22,7 +22,7 @@ const Home = () => {
             <div className="flex flex-row justify-center mb-5 text-4xl text-white font-bold hero-overlay mt-5 py-4">
                 <h2>FAQ</h2>
             </div>
-            <div className="flex flex-col justify-center lg:flex lg:flex-row lg:justify-center gap-4">
+            <div className="flex flex-col items-center lg:flex lg:flex-row lg:justify-center gap-4">
             {
                 parts?.map((part) =>
                  <Faq key={part?._id} part={part} ></Faq>)
